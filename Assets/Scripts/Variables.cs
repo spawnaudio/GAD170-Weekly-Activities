2fa366dfb3ecbf1daa7340da9df22d8d1d0b60c9
squeakyvermin <cody@bloomaudio.work>
2026-03-04 03:44:52 +0800

    Completed Variables

---
+++ Assets/Scripts/Variables.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    public string playerName = "Cody Cork"; // Strings only hold words
+    public int myAge = 31; // Integers only hold whole numbers
+    public float myHealth = 87.5f; // Floats hold numbers with decimals, must end with an "f"
+    public double myHeight = 181.86; // Doubles holds double the float
+    public bool isAlive = true; // Booleans only hold true or false
+    public char myInitial = 'C'; // Chars only hold a single character, must be in single quotes
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
